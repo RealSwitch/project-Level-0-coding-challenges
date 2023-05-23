701582bc07a91892e561800ff0b0979ae78589c5
Sello Makena <smakena74@gmail.com>
2023-05-23 19:29:58 +0000

    added task0.6

--- task0_6.py
+++ task0_6.py
@@ -1,10 +1,9 @@
-def find_maximum(number_1,number_2,number_3):
-    if number_1 > number_2 and number_1 > number_2:
-        return number_1
-    elif number_2 > number_1 and number_2 > number_3:
-        return number_2
-    elif number_3 > number_1 and number_3 > number_2:
-        return number_3
-
+def find_maximum(*integers):
+    maximum = integers[0]
+    for integer in integers:
+        if integer > maximum:
+            maximum = integer
+    return maximum
+    
 if __name__ == "__main__":
-    print(find_maximum(1,2,3))
+    print(find_maximum(1,2,3,9,9,9))
