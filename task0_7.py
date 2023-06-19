1d084603619ef21f6352f47a7a0fa339dd7a93a8
Sello Makena <smakena74@gmail.com>
2023-06-19 06:07:42 +0000

    fixed spelling error

--- task0_7.py
+++ task0_7.py
@@ -1,13 +1,15 @@
-def celsius_to_fehrenheit(celsius):
-    fehrenheit = celsius*(9/5)+32
-    return fehrenheit
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * (9 / 5) + 32
+    return fahrenheit
 
-def fehrenheit_to_celsius(fehrenheit):
-    celsius = (fehrenheit-32)*(5/9)
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * (5 / 9)
     return celsius
 
+
 if __name__ == "__main__":
-    print(celsius_to_fehrenheit(1))
-    print(celsius_to_fehrenheit(32))
-    print(fehrenheit_to_celsius(1))
-    print(fehrenheit_to_celsius(32))
+    print(celsius_to_fahrenheit(1))
+    print(celsius_to_fahrenheit(32))
+    print(fahrenheit_to_celsius(1))
+    print(fahrenheit_to_celsius(32))
