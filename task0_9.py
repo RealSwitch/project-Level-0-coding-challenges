d0eb850aa7259b95541acf8ba1baff7453d94508
Sello Makena <smakena74@gmail.com>
2023-06-19 10:24:33 +0000

    Used f string and debugged to handle uppercases and repetition of vowels in string

--- task0_9.py
+++ task0_9.py
@@ -1,11 +1,13 @@
 def vowels_in_the_string(string):
-    vowels = ["a","e","i","o","u"]
+    vowels = ["a", "e", "i", "o", "u"]
     vowels_in_string = []
-    for character in string:
-        if character in vowels:
+    for character in string.lower():
+        if character in vowels and character not in vowels_in_string:
             vowels_in_string.append(character)
-    print("Vowels:",",".join(vowels_in_string))
+    print(f"Vowels: {','.join(vowels_in_string)}")
+
 
 if __name__ == "__main__":
     vowels_in_the_string("Umuzi")
-    vowels_in_the_string("Amaized")
+    vowels_in_the_string("Amazed")
+    vowels_in_the_string("Omega")
