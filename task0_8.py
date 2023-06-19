38aa76d75b9d57a6a1ed7c6ed3ac09a527f098da
Sello Makena <smakena74@gmail.com>
2023-06-19 06:18:43 +0000

    removed nested loops

--- task0_8.py
+++ task0_8.py
@@ -1,20 +1,15 @@
 def integer_to_time(integer):
-    hours = integer//60
-    minutes = integer%60
-    string_minutes = str(minutes) 
+    hours = integer // 60
+    minutes = integer % 60
+    string_minutes = str(minutes)
     string_hours = str(hours)
-    if hours !=  1:
-        if minutes != 1:
-            return string_hours + " hours, " + string_minutes + " minutes"
-        else:
-            return str(hours) + " hours, " + string_minutes + " minute"
+    if hours != 1 and minutes != 1:
+        return string_hours + " hours, " + string_minutes + " minutes"
+
     else:
-        if minutes != 1:
-            return string_hours + " hour, " + string_minutes + " minutes"
-        else:
-            return string_hours + "hour, " + string_minutes + " minute"
-        
+        return string_hours + " hour, " + string_minutes + " minute"
+
 
 if __name__ == "__main__":
     print(integer_to_time(133))
-    print(integer_to_time(71))
+    print(integer_to_time(221))
