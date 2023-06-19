ba80dd77aa65c39ffb7a107c8b4aa3c7fd21f8ff
Sello Makena <smakena74@gmail.com>
2023-06-19 10:28:10 +0000

    Used f string

--- task0_10.py
+++ task0_10.py
@@ -1,9 +1,10 @@
-def common_characters(string_1,string_2):
+def common_characters(string_1, string_2):
     common_characters = []
     for character in string_1:
         if character in string_2:
             common_characters.append(character)
-    print("Common letters:",",".join(common_characters))
+    print(f"Common letters: {','.join(common_characters)}")
+
 
 if __name__ == "__main__":
-    common_characters("house","computers")
+    common_characters("house", "computers")
