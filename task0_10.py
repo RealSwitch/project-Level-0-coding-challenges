c3b7a3dd1975cff281255c91a341315ed8cfc413
Sello Makena <smakena74@gmail.com>
2023-07-01 02:32:23 +0000

    debugged to return repeated common letters once

--- task0_10.py
+++ task0_10.py
@@ -1,10 +1,14 @@
 def common_characters(string_1, string_2):
-    common_characters = []
-    for character in string_1:
-        if character in string_2:
-            common_characters.append(character)
+    common_characters = list(set(character for character in
+                                 string_1.lower() if character
+                                 in string_2.lower()
+                                 )
+                             )
+
+    common_characters = sorted(common_characters)
     print(f"Common letters: {', '.join(common_characters)}")
 
 
 if __name__ == "__main__":
     common_characters("house", "computers")
+    common_characters("hoOuse", "coomputers")
